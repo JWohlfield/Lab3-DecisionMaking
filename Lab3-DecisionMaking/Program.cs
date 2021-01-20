33f0ecb564c837c45915b2b6921319af0d7b6980
Jeffrey <JWohlfield@hotmail.com>
2021-01-20 16:28:47 -0500

    Updated with changes that were unsaved

--- Lab3-DecisionMaking/Program.cs
+++ Lab3-DecisionMaking/Program.cs
@@ -2,6 +2,23 @@
 
 namespace Lab3_DecisionMaking
 {
+    /*
+    Author:   Jeffrey Wohlfield
+    Date  01-20-2021
+    This program will prompt for their name and a number between 1 and 100.
+
+    Build Specifications:
+    Given an integer entered by a user, perform the following conditional actions:
+        If the integer is odd, print the number entered and “Odd.”
+        If the integer entered is even and in the inclusive range of 2 to 25,
+        print “Even and less than 25.”
+        If the integer entered is even and in the inclusive range of 26 to 60,
+        print “Even.”
+        If the integer entered is even and greater than 60, print the number
+        entered and “Even”.
+        If the integer entered is odd and greater than 60, print the number
+        entered and “Odd.”
+    */
     class Program
     {
         static void Main(string[] args)
@@ -58,12 +75,13 @@ namespace Lab3_DecisionMaking
                 {
                     //The number entered is odd
                     Console.WriteLine($"{input} Odd.");
+                        
+                        // I only do this because of how I interpreted the instructions                        
                         if (input > 60)
-                    {
-                        Console.WriteLine($"{input} Odd.");
-                    }
+                        {   
+                            Console.WriteLine($"{input} Odd.");
+                        }
                 }
-
 
                 // Ask if user wants to continue
                 Console.Write($"\n{userName}, Would you like to continue? (y/n) ");
